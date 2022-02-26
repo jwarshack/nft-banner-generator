791c6add1a5fd97d34e318d9f9fe81b0357a56e9
Jake Warren <jwarshack@gmail.com>
2022-02-25 17:17:34 -0800

    nice

--- src/App.js
+++ src/App.js
@@ -1,49 +1,135 @@
 import logo from './logo.svg';
 import './App.css';
 import figlet from 'figlet';
-import standard from 'figlet/importable-fonts/Standard.js'
-import { FaRegCopy } from 'react-icons/fa'
+import Standard from 'figlet/importable-fonts/Standard'
+import Basic from 'figlet/importable-fonts/Basic'
+import Banner from 'figlet/importable-fonts/Banner';
 
-import { useEffect, useState } from 'react';
+import { FaRegCopy } from 'react-icons/fa'
+import "98.css";
+
+
+
+
+import { useEffect, useRef, useState } from 'react';
+
 
 function App() {
+  const fonts = [
+    {name: "Standard", font: Standard},
+    {name: "Basic", font: Basic},
+    {name: "Banner", font: Banner},
+
+
+
+
+  ]
 
   const [banner, setBanner] = useState()
   const [textToGenerate, setTextToGenerate] = useState()
+  const [minimized, setMinimized] = useState(false)
+
+  const displayRef = useRef()
+
+  function toggleWindow() {
+    const prevValue = minimized
+    console.log(prevValue)
+    setMinimized(!prevValue)
+
+  }
+
 
 
   function generateText() {
-    figlet.parseFont('Standard', standard);
+    figlet.parseFont(fonts[1].name, fonts[1].font);
 
-    figlet.text(textToGenerate, {
-        font: 'Standard',
-    }, function(err, data) {
-        setBanner(data)
+
+    figlet.text(textToGenerate, fonts[1].name,
+     function(err, text) {
+       console.log(err)
+      setBanner(text)
+      console.log(displayRef.current)
+      displayRef.current.style.height = 'inherit'
+      displayRef.current.style.height = `${displayRef.current.scrollHeight}px`
+
     });
+ 
   }
  
   return (
-    <div className="container">
-      <h1 className='logo'>NFT Banner Generator</h1>
-      <input 
-        type="text" 
-        className='input'
-        onChange={(e) => setTextToGenerate(e.target.value)}
-      />
-      <button className="generateButton" onClick={generateText}>Generate!</button>
+    <div className='container'>
       {
-        banner ?
-        <div className='displayContainer'>
-          <textarea type="text" className="display" value={banner} readOnly={true}></textarea>
-          <button className="copyButton" onClick={() => {navigator.clipboard.writeText(banner)}}><FaRegCopy/></button>
+        minimized ?
+        <div className='window' style={{width: '50%'}}>
+          <div className='title-bar'>
+            <div className='title-bar-text'>Ooops..</div>
+            <div className="title-bar-controls">
+              <button aria-label="Minimize" />
+              <button onClick={toggleWindow} aria-label="Maximize" />
+              <button aria-label="Close" />
+            </div>
+          </div>
+          <div className='window-body'>This is awkward...</div>
+
         </div>
         :
-        null
-        
+        <div className='window' style={{width: '50%'}}>
+          <div className='title-bar'>
+            <div className='title-bar-text'>NFT Banner Generator</div>
+            <div className="title-bar-controls">
+              <button onClick={toggleWindow} aria-label="Minimize" />
+              <button aria-label="Maximize" />
+              <button aria-label="Close" />
+            </div>
+          </div>
+          <div className='window-body' style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+
+            <textarea 
+              type="text" 
+              ref={displayRef} 
+              value={banner} 
+              readOnly={true} 
+              disabled={true} 
+              style={{width: "500px", height: "auto", fontFamily: 'sans-serif', textAlign:"center", padding: "0", margin: "0", resize: "none", width: "100%"}}
+              rows="20" 
+            >
+
+            </textarea>
+
+
+            <div style={{display: "flex", alignItems: "center"}}>
+              <input
+                type="range"
+                style={{margin: "20px"}}
+                min="10"
+                max="30"
+                value="11"
+                onChange={(e) => displayRef.current.style.fontSize = `${e.target.value}px`}
+              />
+              <input 
+                  type="text" 
+                  className='input'
+                  onChange={(e) => setTextToGenerate(e.target.value)}
+              />
+              <button onClick={generateText}>Generate</button>
+              <button onClick={() => {navigator.clipboard.writeText(banner)}}><FaRegCopy/></button>
+            </div>
+            <select>
+              <option>5 - Incredible!</option>
+              <option>4 - Great!</option>
+              <option selected>3 - Pretty good</option>
+              <option>2 - Not so great</option>
+              <option>1 - Unfortunate</option>
+            </select>
+          </div>
+        </div>
+
+
       }
 
-      
+
     </div>
+
   );
 }
 
